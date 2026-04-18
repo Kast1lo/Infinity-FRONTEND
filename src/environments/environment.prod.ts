9758edbe8eba0ec941eaad51265510ab4d9384a6
Anton_Kast1lo <152639426+Kast1lo@users.noreply.github.com>
2026-04-18 19:03:23 +0500

    Update production API URL to new endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://infinity-infinitybackend-zae0ns-5d8122-77-110-114-31.traefik.me'
+  apiUrl: 'https://api.infinity-vault.com'
 };
